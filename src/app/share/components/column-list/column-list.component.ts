6ea53cc3b1610e94e6aa7005297b6c6715e9b976
Anton Batalin <antbat2009@gmail.com>
2020-03-21 11:48:46 +0200

    new way nased on search and browser

---
+++ src/app/share/components/column-list/column-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-list',
+  templateUrl: './column-list.component.html',
+  styleUrls: ['./column-list.component.css']
+})
+export class ColumnListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
